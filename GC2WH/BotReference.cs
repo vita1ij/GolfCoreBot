cc40f14e3481f1a90c82cb69bc7c8272cbc9ab63
vita1ij <vitalij.gagunov@gmail.com>
2019-10-18 16:15:04 +0300

    Webhook

---
+++ GC2WH/BotReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace GC2WH
+{
+    public class BotReference
+    {
+        public static TelegramBotClient Bot;
+        public static User BotUser;
+    }
+}
---
+++ GC2WH/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GC2WH.Models;
+
+namespace GC2WH.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Status()
+        {
+            return View();
+        }
+
+        [Route(".well-known/acme-challenge/")]
+        public IActionResult Foo()
+        {
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
