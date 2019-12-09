3d734c98d1035d54861fc4840f233310d0ebe342
vita1ij <vitalij.gagunov@gmail.com>
2019-12-09 18:21:29 +0200

    old

--- GC2WH/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GC2WH.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
